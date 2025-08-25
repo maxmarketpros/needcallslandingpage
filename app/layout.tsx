3232a5cccad21a9b7c8458b4ed054cd2c53918a8
ButteWill <wcruttenden001@student.butte.edu>
2025-08-25 10:57:37 -0700

    add gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17503097114"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17503097114');
+            `,
+          }}
+        />
         <style>{`
 html {
   font-family: ${GeistSans.style.fontFamily};
